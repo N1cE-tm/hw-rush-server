bfca636fd28e5be2bc2ebac0244f3348e0a41302
N1cE™ <banicov2011@yandex.ru>
2023-01-10 20:58:03 +0500

    [fix] Model names

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,11 @@
 export * as query from "./query";
 import { schema as Server } from "./server";
 import { schema as File } from "./file";
-import { schema as NPC } from "./npc";
+import { schema as Npc } from "./npc";
 
-export { Server, File, NPC };
+export { Server, File, Npc };
 export default {
 	Server,
 	File,
-	NPC,
+	Npc,
 };
